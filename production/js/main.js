3b81da8cac69d32c1968feb00870b88d395ed916
volk <nik.babchenko@gmail.com>
2015-09-18 19:16:55 +0300

    added some sections and slider

---
+++ production/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    navigation: true,
+    slideSpeed: 300,
+    paginationSpeed: 300,
+    items: 1,
+    addClassActive: true,
+    nav: true,
+    loop:true,
+    navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+  });
+});
